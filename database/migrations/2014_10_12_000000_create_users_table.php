4e6a3eaff90ea9e5e989406aa873d55d2b29e147
WelintonJLopes <welintonj.lopes@hotmail.com>
2021-12-04 20:54:15 -0300

    Configuração de banco e ajustes de middlewares

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,8 +15,11 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->string('name', 100);
+            $table->string('cpf', 15)->unique();
+            $table->string('cnpj', 15)->unique()->nullable();
+            $table->string('email', 190)->nullable();
+            $table->string('telefone', 12)->unique()->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
